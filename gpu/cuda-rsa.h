144dc455ee2aa104e84773e610de7005a625860c
Brady Thomas <bethomas112@gmail.com>
2014-02-10 00:01:11 -0800

    kernel returning array of index's

--- gpu/cuda-rsa.h
+++ gpu/cuda-rsa.h
@@ -4,10 +4,10 @@
 #include "bigInt.h"
 
 #define NUM_KEYS 256
-#define INT_ARRAY_SIZE 32
+#define INT_ARRAY_SIZE 8
 #define BASE_10 10
 
 
-int setUpKernel(bigInt *arr, uint32_t *bitMatrix);
+int *setUpKernel(bigInt *arr, uint32_t *bitMatrix);
 
 #endif 
